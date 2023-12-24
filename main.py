0ba98ed15dbf2441698b6e46e22e9d121913142c
Emil Grużalski <emil.gruzalski@gmail.com>
2023-12-24 15:13:21 +0100

    Refactor data processing and model evaluation

--- main.py
+++ main.py
@@ -3,52 +3,54 @@ from sklearn.model_selection import train_test_split
 from mlp import MLPClassifier, evaluate_model as evaluate_mlp
 from extra_trees import ExtraTreesClassifierModel, evaluate_model as evaluate_extra_trees
 
-# Wczytanie danych
+# Load data
 data = pd.read_csv('heart.csv')
 
-# Ustawienie ziarna generatora liczb losowych
+# Set the seed for the random number generator
 index_values = data.index.to_numpy()
 seed_value = int(index_values.mean())
 print(f"Seed value: {seed_value}")
 
-# Przetwarzanie danych
+# Data processing
 data['Sex'] = data['Sex'].astype('category')
 data['ChestPainType'] = data['ChestPainType'].astype('category')
-# Usunięcie kolumny 'RestingECG'
+# Removing the 'RestingECG' column
 data = data.drop('RestingECG', axis=1)
-# Usunięcie wiersza gdzie ciśnienie badanego człowieka wynosiło 0, ponieważ wskazuję to że badany jest trupem.
+# Removing the row where the human's blood pressure was 0, as it indicates that the person is deceased.
 data = data[data.RestingBP != 0]
 data['ExerciseAngina'] = data['ExerciseAngina'].astype('category')
 data['ST_Slope'] = data['ST_Slope'].astype('category')
 
-# Konwersja danych kategorialnych na numeryczne
+# Convert categorical data to numerical
 data = pd.get_dummies(data, drop_first=True)
 
-# Podział danych na cechy i etykiety
+# Split data into features and labels
 X = data.drop('HeartDisease', axis=1)
 y = data['HeartDisease']
 
-# Podział danych na zbiory uczące i testowe
+# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed_value)
 
-# Trenowanie modelu MLP
+# Train the MLP model
 mlp_model = MLPClassifier(random_state=seed_value)
 mlp_model.fit(X_train, y_train)
 
-# Ocena modelu MLP
-print("Wyniki dla MLP:")
+# Evaluate the MLP model
+print("Results for MLP:")
 evaluate_mlp(mlp_model, X_test, y_test)
 print("\n")
 
-# Trenowanie modelu Extra Trees
+# Train the Extra Trees model
 et_model = ExtraTreesClassifierModel(random_state=seed_value)
 et_model.fit(X_train, y_train)
 
-# Ocena modelu Extra Trees
-print("Wyniki dla Extra Trees:")
+# Evaluate the Extra Trees model
+print("Results for Extra Trees:")
 evaluate_extra_trees(et_model, X_test, y_test)
+print("\n")
 
+# Feature importances in the Extra Trees model
 importances = et_model.feature_importances()
-print("Ważności cech w modelu Extra Trees:")
+print("Feature importances in the Extra Trees model:")
 for name, importance in zip(X.columns, importances):
     print(f"{name}: {importance}")
--- mlp.py
+++ mlp.py
@@ -7,14 +7,14 @@ class MLPClassifier(SklearnMLPClassifier):
         super().__init__(hidden_layer_sizes=(100,), activation='relu', solver='adam', random_state=random_state)
 
 def evaluate_model(model, X_test, y_test):
-    # Obliczenie współczynników
+    # Calculate metrics
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
 
-    # Narysowanie krzywej ROC
+    # Plot ROC Curve
     fpr, tpr, thresholds = roc_curve(y_test, model.predict_proba(X_test)[:,1])
     roc_auc = auc(fpr, tpr)
 
@@ -29,7 +29,7 @@ def evaluate_model(model, X_test, y_test):
     plt.legend(loc="lower right")
     plt.show()
 
-    # Wyświetlenie wyników
+    # Print metrics
     print(f'Accuracy: {accuracy:.4f}')
     print(f'Recall: {recall:.4f}')
     print(f'Precision: {precision:.4f}')
